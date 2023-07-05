5a06b2e9a0320c476da42d0f478f5d138eeb5b4f
Pablo Ignacio Yáñez Melo <pabloyzm@gmail.com>
2023-07-05 14:59:27 -0400

    updates

--- src/HistogramGeneration/main_generate_histograms.py
+++ src/HistogramGeneration/main_generate_histograms.py
@@ -5,7 +5,8 @@ from tqdm import tqdm
 def generate_hists(csv_path:str,output_path:str, n_clusters:int):
     print(f'Generating histograms using total_words_quant @ {csv_path}')
     #group by image_name and calculate the histogram of visual words
-    for_quatinzation = pd.read_csv(f'{csv_path}/total_words_quant.csv')
+    #for_quatinzation = pd.read_csv(f'{csv_path}/total_words_quant.csv')
+    for_quatinzation = pd.read_csv(f'{csv_path}/total_words_kmeans.csv')
     for_quatinzation = for_quatinzation.drop(columns = ['index', 'class'])
     #print(for_quatinzation.iloc[0])
     for_quatinzation = for_quatinzation.groupby('image_name').agg(lambda x: x.tolist())
@@ -14,6 +15,7 @@ def generate_hists(csv_path:str,output_path:str, n_clusters:int):
     for_quatinzation['histogram'] = for_quatinzation['visual_word'].apply(lambda x: np.histogram(np.array(x).astype(float), bins = n_clusters, density=True)[0])
     #print(for_quatinzation['visual_word'])
     for_quatinzation = for_quatinzation.drop(columns = ['visual_word'])
-    for_quatinzation.to_csv(f'{output_path}/total_quant_histogram.csv', index = False)
+    #for_quatinzation.to_csv(f'{output_path}/total_quant_histogram.csv', index = False)
+    for_quatinzation.to_csv(f'{output_path}/total_kmeans_histogram.csv', index=False)
     print(f'Output has been saved as total_quant_histogram.csv @ {output_path}')
  
